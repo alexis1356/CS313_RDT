d15511de97ed314d13441d75c122b40ba2df71d1
silvi <silvi.koycheva.2019@uni.strath.ac.uk>
2021-10-13 16:40:05 +0200

    Project structure

---
+++ src/TransportLayerPacket.java
@@ -0,0 +1,30 @@
+package src;
+
+public class TransportLayerPacket {
+
+    // Maybe remove these
+    // You may need extra fields
+    private int seqnum;
+    private int acknum;
+
+    byte[] data;
+
+    // You may need extra methods
+
+    public TransportLayerPacket(TransportLayerPacket pkt) {
+        // complete this method
+    }
+
+    public void setSeqnum(int seqnum) {
+        this.seqnum = seqnum;
+    }
+
+    public void setAcknum(int acknum) {
+        this.acknum = acknum;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+}
