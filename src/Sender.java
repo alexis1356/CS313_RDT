6a3a2c3ed323e4ae1ce0573ae9a35747640b9b0a
Rachael Espie <rachael.espie.2019@uni.strath.ac.uk>
2021-10-22 16:23:10 +0100

    Stop-and-wait working okay - RDT 2.0 completed

--- src/Sender.java
+++ src/Sender.java
@@ -2,13 +2,11 @@ package src;
 
 public class Sender extends TransportLayer{
     private TransportLayerPacket packet;
-    //private boolean isAck = true;
-    private int[] states = {1,2}; //lists the states of sender as ints so
     // state 1 - means we are waiting from a call from above - can send packets
     // state 2 - means we are waiting for an ACk or NAK - can't send packets
-    private int currentState = 1; //int of what state we are currently on
+    private int currentState = 1; //int of what state we are currently on - see above for what this means
     private int nextData = 0; //index of the next data to be sent
-    private int nextFreeIndex = 0;
+    private int nextFreeIndex = 0; // index of next free index in dataQueue
     private byte[][] dataQueue = new byte[10][]; //list of data that needs to be sent;
 
     public Sender(String name, NetworkSimulator simulator)
@@ -18,14 +16,14 @@ public class Sender extends TransportLayer{
 
     @Override
     public void init() {
-        currentState = 1;
+        currentState = 1; //initialises current state to 1
         //        TODO
     }
 
     @Override
     public void rdt_send(byte[] data){
         switch (currentState){
-            case 1:
+            case 1: // we can send packets
                 byte sum = 0;
                 for (int i = 0; i < data.length; i++) {
                     sum += data[i];
@@ -33,18 +31,14 @@ public class Sender extends TransportLayer{
                 sum ^= 0xFFFFFFFF;
                 packet = new TransportLayerPacket(0, 0, data, sum);
                 simulator.sendToNetworkLayer(this, packet);
-                //System.out.println(Integer.toBinaryString((corruptedSum & 0xFF) + 0x100).substring(1));
                 System.out.println("Sent: " + Integer.toBinaryString((sum & 0xFF) + 0x100).substring(1));
                 currentState =2;
-            case 2:
-                 //nextData = data;
+                break;
+            case 2: // we are waiting for acknowledgment
                     dataQueue[nextFreeIndex] = data;
                     nextFreeIndex++;
         }
-
     }
-
-
 
     @Override
     public void rdt_receive(TransportLayerPacket pkt) {
@@ -56,14 +50,11 @@ public class Sender extends TransportLayer{
         else {
             System.out.println("Received ACK");
             currentState=1;
-            if (pkt.getData() != dataQueue[nextData]) {
-                System.out.println("Next data: " + nextData +" Next Free Index: "+ nextFreeIndex);
+            if (pkt.getData() != dataQueue[nextData] && dataQueue[nextData] != null) {
                 rdt_send(dataQueue[nextData]);
                 nextData++;
             }
         }
-        //simulator.sendToApplicationLayer(this, data);
-
     }
 
     @Override
